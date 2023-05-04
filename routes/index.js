76707e7e1105c1f82cc2e4fc3a921c97c418671d
AaronMadera <aaron.madera06@gmail.com>
2023-05-03 22:09:20 -0600

    Feat-2 Add serverless handler (#2)
    * add post route
    
    * add serverless-express

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.send("Hello there!");
+router.get('/', function (req, res) {
+    res.send("Hello there!");
+});
+
+router.post('/', function (req, res) {
+    res.status(200).json({
+        payload: req.body,
+        time: + new Date(),
+    })
 });
 
 module.exports = router;
