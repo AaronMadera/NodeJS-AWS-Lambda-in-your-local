954fa6d49169cb5940c477bdc1f5db4e98b1c398
AaronMadera <aaron.madera06@gmail.com>
2023-05-03 22:03:41 -0600

    add serverless-express

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const { eventContext } = require('@vendia/serverless-express/src/middleware');
 
 const indexRouter = require('./routes/index');
 
@@ -8,6 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// middleware config to let us parse the AWS Lambda event into `apiGateway`
+app.use(eventContext());
 
 app.use('/', indexRouter);
 
---
+++ lambda.js
@@ -0,0 +1,19 @@
+// lambda.js
+const serverlessExpress = require('@vendia/serverless-express')
+const app = require('./app')
+
+// AWS Lambda can use same serverless-express instance for all concurrent running lambdas
+let serverlessInstance;
+
+function setup(event, context) {
+    serverlessInstance = serverlessExpress({ app });
+    return serverlessInstance(event, context);
+}
+async function handler(event, context) {
+    if (serverlessInstance) {
+        return serverlessInstance(event, context);
+    }
+    return setup(event, context)
+}
+
+exports.handler = handler
