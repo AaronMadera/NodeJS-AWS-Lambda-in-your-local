533352ab8b485cd5b47bace0ec0b64a17cc96dde
AaronMadera <aaron.madera06@gmail.com>
2023-05-13 18:13:44 -0600

    Feat-3 Using AWS Lambda image (#3)
    * adds docker image of aws lambda
    
    * adds makefile for useful commands
    
    * adds example of lambda events
    
    * adds parser for req.body issue
    
    * adds ip route
    
    * fix Makefile
    
    * format console.log

--- lambda.js
+++ lambda.js
@@ -1,6 +1,6 @@
 // lambda.js
-const serverlessExpress = require('@vendia/serverless-express')
-const app = require('./app')
+const serverlessExpress = require('@vendia/serverless-express');
+const app = require('./app');
 
 // AWS Lambda can use same serverless-express instance for all concurrent running lambdas
 let serverlessInstance;
@@ -10,10 +10,11 @@ function setup(event, context) {
     return serverlessInstance(event, context);
 }
 async function handler(event, context) {
+    console.log('event: %o', event);
     if (serverlessInstance) {
         return serverlessInstance(event, context);
     }
-    return setup(event, context)
+    return setup(event, context);
 }
 
-exports.handler = handler
+exports.handler = handler;
